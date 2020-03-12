af6affa4ff8732d40838ee51ce5835dbe0a0a0fc
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 15:06:44 +1000

    Add booleans and comparisons

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,28 @@
-let age;
+console.log(true, false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+let email = 'alex@xxx.com';
+let names = ['mario', 'luigi', 'toad'];
 
-let year = null;
+let result1 = email.includes('@');
+let result2 = names.includes('browser');
+console.log(result1);
+console.log(result2);
 
-console.log(year, year + 3, `the year is ${year}`);
+//comparison operators 
+let age = 25;
+console.log('number comparisons');
+console.log(age == 25);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
+
+let name = 'alex';
+console.log('string comparisons');
+console.log(name == 'alex');
+console.log(name == 'Alex');
+console.log(name > 'crystal');
+console.log(name > 'Alex');
+console.log(name > 'Crystal');
