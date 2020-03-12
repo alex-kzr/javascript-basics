94f1262d94fac579bde34ff48c6768e54f303acd
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 23:04:18 +1000

    Add arrow functions

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,22 @@
+// regular function
 const calcArea = function(radius){
     return 3.14 * radius**2;    
-}
+};
 
-const area = calcArea(5);
-console.log(area);
+// arrow function
+const calcVol = (area, height) => {
+    return area * height;
+};
+console.log(calcVol(calcArea(5), 5));
+
+const greet = name => `Hello ${name}`;
+console.log(greet('alex'));
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+console.log(bill([10,15,30], 0.2));
