0f361b822eff23ef15eee0d4e04a32ac56ea3221
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 22:20:36 +1000

    Add switch statement

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,40 @@
-const scores = [50,25,0,30,100,20,10];
-for(let i = 0; i < scores.length; i++){
-    if(scores[i] === 0){
-        continue;
-    }
-    
-    console.log('your score: ', scores[i]);
-    if(scores[i] === 100){
-        console.log('congrats, you got the top score!');
+const grade = 'C';
+
+switch(grade){
+    case 'A':
+        console.log('you gon an A!');
         break;
-    }
+    case 'B':
+        console.log('you gon an B!');
+        break;
+    case 'C':
+        console.log('you gon an C!');
+        break;
+    case 'D':
+        console.log('you gon an D!');
+        break;
+    case 'E':
+        console.log('you gon an E!');
+        break;
+    default:
+        console.log('not a valid grade')
 }
+
+
+
+
+
+// using if else statement
+// if(grade === 'A'){
+
+// } else if(grade === 'B'){
+
+// } else if(grade === 'C'){
+    
+// } else if(grade === 'D'){
+    
+// } else if(grade === 'E'){
+    
+// } else {
+    
+// } 
