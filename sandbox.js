88f228a12d25bfdf32dfc599548961a35d77e5b4
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 14:55:10 +1000

    Add udefined and null

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,7 @@
-let ninjas = ['alex', 'ryu', 'chun-li'];
-ninjas[1] = 'ken';
-console.log(ninjas);
-console.log(ninjas[1]);
+let age;
 
-let ages = [20,25,30,35];
-console.log(ages[2]);
+console.log(age, age + 3, `the age is ${age}`);
 
-let random = ['alex','crystal', 30, 20];
-console.log(random);
+let year = null;
 
-console.log(ninjas.length);
-
-// array methods
-let result = ninjas.join('-');
-console.log(result);
-
-let index = ninjas.indexOf('chun-li');
-console.log(index); 
-
-let concatResult = ninjas.concat(['mario','crystal']);
-console.log(concatResult);
-
-let pushResult = ninjas.push('mario');
-console.log(pushResult, ninjas);
-
-let popResult = ninjas.pop();
-console.log(popResult, ninjas);
+console.log(year, year + 3, `the year is ${year}`);
