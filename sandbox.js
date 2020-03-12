0ed79ca388268a42ee6b0a4c9f0b57abe02f10fb
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 22:12:05 +1000

    Add break and continue

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,12 @@
-let user = false;
-if(!user){
-    console.log('you must be logged in to continue');
+const scores = [50,25,0,30,100,20,10];
+for(let i = 0; i < scores.length; i++){
+    if(scores[i] === 0){
+        continue;
+    }
+    
+    console.log('your score: ', scores[i]);
+    if(scores[i] === 100){
+        console.log('congrats, you got the top score!');
+        break;
+    }
 }
