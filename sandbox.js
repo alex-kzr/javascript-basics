1b87ca4ae6837b55ad3c7b2b9ef3fe0258a05dae
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 13:23:40 +1000

    Add variables and contant

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,14 @@
-alert('hello, world!');
+let age = 25;
+let year = 2020;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50; - error
+console.log(points);
+
+var score = 75;
+console.log(score);
