6ff9f94c0103d2758ea3a4c590af688c580a4267
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 21:55:28 +1000

    Add if statement

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,15 @@
-let i = 5;
-do{
-    console.log('val of i is:', i);
-    i++;
-}while(i < 5)
-console.log('loop finished');
+const age = 25;
+
+if(age > 20){
+    console.log('you are over 20 years old');
+}
+
+const ninjas = ['alex', 'ryu', 'chun-li', 'yoshi'];
+if(ninjas.length > 3){
+    console.log("that's a lot of ninjas");
+}
+
+const password = 'password';
+if(password.length >= 8){
+    console.log('that password is long enough!');
+}
