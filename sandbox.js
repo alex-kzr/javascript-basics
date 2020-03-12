557aa21630bfa74d3d295718e93d38509366afdb
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 22:39:02 +1000

    Add function declaration and expression

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,11 @@
-let age = 30;
+const speak = function(){
+    console.log('good day!');
+};
 
-if(true){
-    let age = 40;
-    let name = 'alex';
-    console.log('inside 1s code block: ', age, name);
+speak(); // can't call before declaration
 
-    if(true){
-        let age = 50;
-        console.log('inside 2nd code block: ', age, name);
-        var msg = 'hello';
-    }
+greet();
+
+function greet(){
+    console.log('hello there!');
 }
-
-console.log('outside code block: ', age, name, msg);
