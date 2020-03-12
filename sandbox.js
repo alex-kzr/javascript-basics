3b2a2aa74c71292a5c2c64b6704cfd9a50c086f8
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 21:59:16 +1000

    Add else and else if statement

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,9 @@
-const age = 25;
+const password = 'passaljfalfjaljf';
 
-if(age > 20){
-    console.log('you are over 20 years old');
-}
-
-const ninjas = ['alex', 'ryu', 'chun-li', 'yoshi'];
-if(ninjas.length > 3){
-    console.log("that's a lot of ninjas");
-}
-
-const password = 'password';
-if(password.length >= 8){
+if(password.length >= 12){
+    console.log('that password is might strong');
+}else if(password.length >= 8){
     console.log('that password is long enough!');
+}else{
+    console.log('password is not long enough');
 }
