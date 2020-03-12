0722058a6f7bbe1cfb4b0233d5beb15d564f271f
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 22:51:43 +1000

    Add returning value from function

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,6 @@
-const speak = function(name = 'luigi', time = 'night'){
-    console.log(`good ${time} ${name}`);
-};
+const calcArea = function(radius){
+    return 3.14 * radius**2;    
+}
 
-speak();
-speak('alex');
-speak('mario', 'morning');
+const area = calcArea(5);
+console.log(area);
