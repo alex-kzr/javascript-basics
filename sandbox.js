f747440b9edea86bdf5bf5909ef39ee576387c1d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-13 00:00:18 +1000

    Add methods to the object

--- sandbox.js
+++ sandbox.js
@@ -3,19 +3,13 @@ let user = {
     age: 30,
     email: 'crystal@xxx.com',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user login');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    }
 }
 
-console.log(user);
-console.log(user.name);
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-user.age = 35;
-console.log(user.age);
-
-const key = 'location';
-console.log(user[key]);
-
-console.log(typeof user);
+user.login();
