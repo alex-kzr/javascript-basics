3d4c6098d10ace47c54d9c052a18cd9fcb369f67
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 16:19:40 +1000

    Add do while loop

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,6 @@
-// let i = 0;
-// while(i < 5){
-//     console.log('in loop:', i);
-//     i++;
-// }
-// console.log('loop finished');
-
-const names = ['alex', 'mario', 'luigi'];
-let i = 0;
-while(i < names.length){
-    console.log(names[i]);
+let i = 5;
+do{
+    console.log('val of i is:', i);
     i++;
-}
+}while(i < 5)
+console.log('loop finished');
