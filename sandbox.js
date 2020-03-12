0f586a65862f98ccfae4995b3a6ffd622cefea7d
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 15:50:59 +1000

    Add loose and strict comparison

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,13 @@
-console.log(true, false, 'true', 'false');
-
-// methods can return booleans
-let email = 'alex@xxx.com';
-let names = ['mario', 'luigi', 'toad'];
-
-let result1 = email.includes('@');
-let result2 = names.includes('browser');
-console.log(result1);
-console.log(result2);
-
-//comparison operators 
 let age = 25;
-console.log('number comparisons');
-console.log(age == 25);
-console.log(age != 30);
-console.log(age > 20);
-console.log(age < 20);
-console.log(age <= 25);
-console.log(age >= 25);
 
-let name = 'alex';
-console.log('string comparisons');
-console.log(name == 'alex');
-console.log(name == 'Alex');
-console.log(name > 'crystal');
-console.log(name > 'Alex');
-console.log(name > 'Crystal');
+// loose comparison (different types can still be equal)
+console.log('loose', age == 25);
+console.log('loose', age == '25');
+console.log('loose', age != 25);
+console.log('loose', age != '25');
+
+// strict comparison (different tyeps cannot be equal)
+console.log('strict', age === 25);
+console.log('strict', age === '25');
+console.log('strict', age !== 25);
+console.log('strict', age !== '25');
