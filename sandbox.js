aa96e8d7f21692587d00ff4cc752d43fa9df1a0f
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-12 22:07:06 +1000

    Add logical NOT

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,4 @@
-const password = 'passw@';
-
-if(password.length >= 12 && password.includes('@')){
-    console.log('that password is might strong');
-}else if(password.length >= 8 || password.includes('@') && password.length > 5){
-    console.log('that password is strong enough!');
-}else{
-    console.log('password is not strong enough');
+let user = false;
+if(!user){
+    console.log('you must be logged in to continue');
 }
