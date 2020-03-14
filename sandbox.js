93d2705e249b4b4f6ef27e5f95aa55501d54f9bb
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 01:03:35 +1000

    Add chaining array methods

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,17 @@
-// sorting strings
-const names = ['mario', 'alex', 'chun-li', 'yoshi', 'luigi'];
-const sortedNames = names.sort();
-console.log(sortedNames);
-
-names.sort();
-console.log(names);
-
-names.reverse();
-console.log(names);
-
-// sorting numbers
-const points = [10, 5, 0, 40, 60, 10, 20, 70];
-const stringSortedPoints = [...points]; 
-stringSortedPoints.sort();
-console.log(stringSortedPoints);
-
-stringSortedPoints.reverse();
-console.log(stringSortedPoints);
-
-const numberSortedPoints = [...points];
-numberSortedPoints.sort((a,b) => b - a); // descending sorting
-numberSortedPoints.sort((a,b) => a - b); // ascending sorting
-console.log(numberSortedPoints); 
-
-const firstHighPoint = points.find(score => score > 50);
-
-console.log(firstHighPoint);
-
-const players = [
-    {player: 'mario', score: 20},
-    {player: 'yoshi', score: 10},
-    {player: 'luigi', score: 50},
-    {player: 'crystal', score: 30},
-    {player: 'alex', score: 70}
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red shell', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50}
 ]
 
-const descPlayers = [...players];
-// descPlayers.sort((a, b) => {
-//     if(a.score > b.score){
-//         return -1;
-//     }else if(b.score > a.score){
-//         return 1;
-//     }else{
-//         return 0;
-//     }
-// }); 
+// const filteredProducts = products.filter(product => product.price > 20);
 
-descPlayers.sort((a,b) => b.score - a.score); // descending sorting
+// const promos = filteredProducts.map(product => `the ${product.name} is ${product.price / 2} pounds`);
 
-console.log(descPlayers, players);
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the ${product.name} is ${product.price / 2} pounds`);
 
-const ascPlayers = [...players];
-ascPlayers.sort((a, b) => a.score - b.score); // ascending sorting
-
-console.log(ascPlayers, players);
+console.log(promos);
