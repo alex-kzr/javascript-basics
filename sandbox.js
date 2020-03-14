f4543f47b689f9c62961954a2ae9c8d476ebfb9e
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-14 23:44:17 +1000

    Add map array method

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,23 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+const prices = [20, 10, 30, 25, 40, 80, 5];
 
-const filteredScores = scores.filter(score => {
-    return score > 20;
+const salePrices = prices.map(price => price / 2);
+
+console.log(salePrices);
+
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50}
+]
+
+const saleProducts = products.map(product => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2}
+    }else{
+        return product
+    }
 });
 
-console.log(filteredScores);
-
-const users = [
-    { name: 'alex', premium: true },
-    { name: 'yoshi', premium: false },
-    { name: 'mario', premium: false },
-    { name: 'chun-li', premium: true }
-];
-
-const premiumUsers = users.filter(user => user.premium);
-
-console.log(premiumUsers);
+console.log(saleProducts, products);
